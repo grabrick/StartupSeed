f889e241fcf091a45cb9efe5f0cf7ddecc223e39
grabrick <grabrick@mail.ru>
2023-08-12 01:26:34 +0600

    hotfix: fixed node router

--- index.js
+++ index.js
@@ -15,25 +15,24 @@ app.use(bodyParser.json({ limit: '50mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(bodyParser.text({ limit: '200mb' }))
 app.use('/uploads', express.static(__dirname + '/uploads'));
-// app.use('/uploads', express.static('uploads'));
 app.use(compression());
 app.use(cors());
 
 // Localhost
-app.use('/api/auth', require('./server/routes/authRouter'))
-app.use('/api', require('./server/routes/verifyRouter'))
-app.use('/api', require('./server/routes/projectRouter'))
-app.use('/api', require('./server/routes/specialistRouter'))
-app.use('/api', require('./server/routes/favoriteRouter'))
-app.use('/api', require('./server/routes/messengerRouter'))
+// app.use('/api/auth', require('./server/routes/authRouter'))
+// app.use('/api', require('./server/routes/verifyRouter'))
+// app.use('/api', require('./server/routes/projectRouter'))
+// app.use('/api', require('./server/routes/specialistRouter'))
+// app.use('/api', require('./server/routes/favoriteRouter'))
+// app.use('/api', require('./server/routes/messengerRouter'))
 
 // DEPLOY //
-// app.use('/auth', require('./server/routes/authRouter'))
-// app.use('/', require('./server/routes/verifyRouter'))
-// app.use('/', require('./server/routes/projectRouter'))
-// app.use('/', require('./server/routes/specialistRouter'))
-// app.use('/', require('./server/routes/favoriteRouter'))
-// app.use('/', require('./server/routes/messengerRouter'))
+app.use('/auth', require('./server/routes/authRouter'))
+app.use('/', require('./server/routes/verifyRouter'))
+app.use('/', require('./server/routes/projectRouter'))
+app.use('/', require('./server/routes/specialistRouter'))
+app.use('/', require('./server/routes/favoriteRouter'))
+app.use('/', require('./server/routes/messengerRouter'))
 
 async function start() {
   try {
